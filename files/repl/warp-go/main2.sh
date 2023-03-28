e3e3ddfae416662d36e534d66a69807848a450a4
MisakaNo の 小破站 <misakablog@protonmail.com>
2023-03-28 09:57:37 +0000

    Update 2 files
    - /files/repl/warp-go/main2.sh
    - /files/repl/wgcf/main2.sh

--- files/repl/warp-go/main2.sh
+++ files/repl/warp-go/main2.sh
@@ -29,7 +29,17 @@ echo -e " ${GREEN}3.${PLAIN} WARP Teams"
 echo ""
 read -p "请输入选项 [1-3]: " account_type
 if [[ $account_type == 2 ]]; then
-  echo "ok"
+  yellow "获取 CloudFlare WARP 账号密钥信息方法: "
+  green "电脑: 下载并安装 CloudFlare WARP → 设置 → 偏好设置 → 账户 →复制密钥到脚本中"
+  green "手机: 下载并安装 1.1.1.1 APP → 菜单 → 账户 → 复制密钥到脚本中"
+  echo ""
+  yellow "重要：请确保手机或电脑的 1.1.1.1 APP 的账户状态为WARP+！"
+  echo ""
+  read -rp "输入 WARP 账户许可证密钥 (26个字符): " warpkey
+  until [[ $warpkey =~ ^[A-Z0-9a-z]{8}-[A-Z0-9a-z]{8}-[A-Z0-9a-z]{8}$ ]]; do
+    red "WARP 账户许可证密钥格式输入错误，请重新输入！"
+    read -rp "输入 WARP 账户许可证密钥 (26个字符): " warpkey
+  done
 elif [[ $account_type == 3 ]]; then
   echo "ok"
 else
