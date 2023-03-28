596358a644b36e42d8731ede60ec731d2d3139d7
MisakaNo の 小破站 <misakablog@protonmail.com>
2023-03-28 09:54:46 +0000

    Update 2 files
    - /files/repl/wgcf/main2.sh
    - /files/repl/warp-go/main2.sh

---
+++ files/repl/warp-go/main2.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+PLAIN='\033[0m'
+
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+
+rm -f warp.conf proxy.conf
+
+chmod +x ./warp-go
+clear
+yellow "请选择需要使用的 WARP 账户类型"
+echo ""
+echo -e " ${GREEN}1.${PLAIN} WARP 免费账户 ${YELLOW}(默认)${PLAIN}"
+echo -e " ${GREEN}2.${PLAIN} WARP+"
+echo -e " ${GREEN}3.${PLAIN} WARP Teams"
+echo ""
+read -p "请输入选项 [1-3]: " account_type
