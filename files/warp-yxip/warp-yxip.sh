da812e69973d23e927555efd31bb1825ebf9959e
MisakaNo の 小破站 <misakablog@protonmail.com>
2023-04-04 02:11:08 +0000

    Update file warp-yxip.sh

--- files/warp-yxip/warp-yxip.sh
+++ files/warp-yxip/warp-yxip.sh
@@ -17,28 +17,6 @@ yellow() {
     echo -e "\033[33m\033[01m$1\033[0m"
 }
 
-REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'" "fedora" "alpine")
-RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Fedora" "Alpine")
-PACKAGE_UPDATE=("apt-get update" "apt-get update" "yum -y update" "yum -y update" "yum -y update" "apk update -f")
-PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "yum -y install" "apk add -f")
-PACKAGE_UNINSTALL=("apt -y autoremove" "apt -y autoremove" "yum -y autoremove" "yum -y autoremove" "yum -y autoremove" "apk del -f")
-
-CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
-
-[[ $EUID -ne 0 ]] && nesudo="sudo"
-
-for i in "${CMD[@]}"; do
-    SYS="$i" && [[ -n $SYS ]] && break
-done
-
-for ((int = 0; int < ${#REGEX[@]}; int++)); do
-    if [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]]; then
-        SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
-    fi
-done
-
-[[ -z $SYSTEM ]] && red "不支持当前VPS系统, 请使用主流的操作系统" && exit 1
-
 archAffix(){
     case "$(uname -m)" in
         i386 | i686 ) echo '386' ;;
@@ -49,13 +27,7 @@ archAffix(){
     esac
 }
 
-endpointyx(){
-    # 安装系统必需依赖
-    if [[ ! $SYSTEM == "CentOS" ]]; then
-        $nesudo ${PACKAGE_UPDATE[int]}
-    fi
-    $nesudo ${PACKAGE_INSTALL[int]} openssh-server bash wget
-    
+endpointyx(){    
     # 下载优选工具软件，感谢某匿名网友的分享的优选工具
     wget https://gitlab.com/Misaka-blog/warp-script/-/raw/main/files/warp-yxip/warp-linux-$(archAffix) -O warp
     
@@ -232,4 +204,5 @@ menu(){
         * ) endpoint4 ;;
     esac
 }
+
 menu
