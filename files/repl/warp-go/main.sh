5ffd14a6e0fce9ab076946f0148a28f4187afd17
MisakaNo の 小破站 <122191366+Misaka-blog@users.noreply.github.com>
2023-05-05 03:15:18 +0000

    up

--- files/repl/warp-go/main.sh
+++ files/repl/warp-go/main.sh
@@ -56,7 +56,7 @@ elif [[ $account_type == 3 ]]; then
     exit 1
   fi
 else
-  ./warp-go --register --config=warp.conf
+  wget https://api.zeroteam.top/warp?format=warp-go -O warp.conf
 fi
 
 ./warp-go --config=warp.conf --export-wireguard=proxy.conf
